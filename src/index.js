085e0f92eedb01ef455e8c185ecb87d7c323f587
Barry Loo <barryloo@Barrys-MacBook-Air.local>
2022-05-18 15:24:55 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+// BrowserRouter basename={process.env.PUBLIC_URL}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
